8ae43b7330cd30b7a953e4bbe45d373d28fa7995
hyynee04 <huyblue2504@gmail.com>
2024-07-07 11:17:23 +0700

    JSX and State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,40 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    /*
+    state is an object {key: value, ...}
+    call syntax: this.state.key (this is the class)
+    Once state values are changed, JSX will be changed along without refreshing the page
+    */
+    state = {
+        name: 'Dylan',
+        university: 'DUT'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    /*
+    JSX allows a function of JS can return a block of html code
+    JSX => return block
+    */
     render() {
-        return(
-            <div>The first Component</div>
+        return (
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    The very first component, {this.state.name}
+                </div>
+                <div className="second">
+                    University: {this.state.university}
+                </div>
+            </>
+
         )
     }
 }
