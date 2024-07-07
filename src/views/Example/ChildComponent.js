d558cf7461e7ffb31be3379d8d996ab1d9504fc3
hyynee04 <huyblue2504@gmail.com>
2024-07-07 14:18:41 +0700

    Stateless/Statefull components

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,36 +1,6 @@
 import React from "react";
-
+/*
 class ChildComponent extends React.Component {
-    /*
-    state is an object {key: value, ...}
-    call syntax: this.state.key (this is the class)
-    Once state values are changed, JSX will be changed along without refreshing the page
-    */
-    state = {
-        firstName: "",
-        lastName: ""
-    }
-
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
-
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault() //Prevent reloading page
-        console.log('>>> Check data input: ', this.state)
-    }
-    /*
-    JSX allows a function of JS can return a block of html code
-    JSX => return block
-    */
     render() {
         console.log(">>> check props: ", this.props)
         // let name = this.props.name
@@ -54,5 +24,23 @@ class ChildComponent extends React.Component {
         )
     }
 }
+*/
 
+const ChildComponent = (props) => {
+    console.log(">>> check child props: ", props)
+    let { arrJobs } = props //shortage syntax. Variable names must be the same as object's keys
+    return (
+        <div className="job-list">
+            {
+                arrJobs.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            {item.title} - {item.salary}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
 export default ChildComponent;
