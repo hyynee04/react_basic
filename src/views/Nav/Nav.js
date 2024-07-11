dcc4bf79d6fbe8d7674461a7b937f1237be2e06a
hyynee04 <huyblue2504@gmail.com>
2024-07-11 17:07:43 +0700

    End (cause of version error)

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,14 +1,21 @@
 import React from "react";
 import './Nav.scss'
+import {
+    BrowserRouter,
+    NavLink,
+    Link
+} from "react-router-dom";
 
 class Nav extends React.Component {
     render() {
         return (
-            <div className="topnav">
-                <a className="active" href="/">Home</a>
-                <a href="/todo">Todo</a>
-                <a href="/about">About</a>
-            </div>
+            <BrowserRouter>
+                <div className="topnav">
+                    <Link to="/">Home</Link>
+                    <Link to="/todo">Todo App</Link>
+                    <Link to="/about">About</Link>
+                </div>
+            </BrowserRouter>
         )
     }
 }
